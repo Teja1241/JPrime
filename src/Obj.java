69d02c491ae66de14ae0513cb618480732109f22
ABDharani <dharanitejakumar@gmail.com>
2018-03-04 23:31:29 -0600

    Second Commit

---
+++ src/Obj.java
@@ -0,0 +1,10 @@
+
+public class Obj {
+
+	public static void main(String[] args)
+	{
+	ObjectEx A = new ObjectEx();
+	System.out.println("Object variable in non static class is" + A.i);
+	}
+
+}
---
+++ src/ObjectEx.java
@@ -0,0 +1,48 @@
+
+public class ObjectEx 
+{
+	int i=10; //object variable
+	
+	public static void Var() 
+	{
+	 int Final =25;
+	
+	
+	}
+	
+	
+	
+	public static void main(String args[]) 
+	{
+		ObjectEx A = new ObjectEx();// Creating an obect for the class
+		System.out.println("Object variable is" + A.i);
+		
+		StaObject();
+		{
+			System.out.println("The static value of i is" + Var.Final);
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+
+
+	private static void StaObject() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	private static void object() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
