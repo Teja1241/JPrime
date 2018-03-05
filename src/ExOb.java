193ca08f1320a5983c5981acb1f4a3d89fa73368
ABDharani <dharanitejakumar@gmail.com>
2018-03-05 15:56:17 -0600

    third commit

---
+++ src/ExOb.java
@@ -0,0 +1,35 @@
+
+
+public class ExOb {
+
+	public static void main(String args[])
+	
+	{
+		
+	int c = ObjectEx.n; // calling static value of n from other class
+	System.out.println("value of static n of other class is " + c);	
+	
+	
+	ObjectEx Obj = new ObjectEx(); // creating an object for other class object level variable
+	System.out.println("the value of obj is " + Obj.i);
+	
+	
+	int a = ObjectEx.s; // defining protected value from other class
+	System.out.println("Protected variable value from other class in the same package" + a);
+	
+	ObjectEx.temp(); // accessing static method from other class from same package
+	
+	
+	
+	
+	
+	
+	
+	/*System.out.println("tshdsi " + ObjectEx.j);  Tried to
+	 acess j variable which is defined in ObjectEx class but comes up with an 
+	 error like j is not visible */
+	
+	
+	}
+
+}
--- src/ObjectEx.java
+++ src/ObjectEx.java
@@ -1,48 +1,34 @@
 
 public class ObjectEx 
 {
+	
 	int i=10; //object variable
 	
-	public static void Var() 
+	public static int n=99;
+	private static int j=25;
+	protected static int s = 10;
+	
+	
+	public static int temp() 
 	{
-	 int Final =25;
-	
-	
+	 int g =25;
+	 System.out.println("int g is" + g);
+	 return (g);
 	}
 	
-	
-	
+		
 	public static void main(String args[]) 
 	{
-		ObjectEx A = new ObjectEx();// Creating an obect for the class
+		ObjectEx A = new ObjectEx();// Creating an object for the class
 		System.out.println("Object variable is" + A.i);
+		int y=29;
 		
-		StaObject();
-		{
-			System.out.println("The static value of i is" + Var.Final);
+		System.out.println("This is "+" " + ObjectEx.n); //static variable in object level 
+
+		ObjectEx.temp();  // calling object method in object level
+
+		
+		
 			
-			
-		}
-		
-		
-		
-		
-		
-		
 	}
-
-
-
-	private static void StaObject() {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-
-	private static void object() {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
